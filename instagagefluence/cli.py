1f6ffbe48982ab7d245ab0247404e70f7d2bf952
Kyle Walker <KyleJamesWalker@gmail.com>
2018-10-21 12:29:31 -0700

    Working Login, with insecure cache

--- instagagefluence/cli.py
+++ instagagefluence/cli.py
@@ -1,5 +1,11 @@
 import argparse
+import hashlib
+import os
+import pickle
+import time
 import yamlsettings
+
+from InstagramAPI import InstagramAPI
 
 
 def main():
@@ -15,4 +21,23 @@ def main():
     args = parser.parse_args()
 
     settings = yamlsettings.load('igf://{}'.format(args.config))
-    print(settings)
+
+    user_data_file = '{}{}.data'.format(
+        settings.data_prefix,
+        hashlib.md5(settings.username.encode('utf-8')).hexdigest()
+    )
+
+    if os.path.isfile(user_data_file):
+        print("Using existing data")
+        ig = pickle.load(open(user_data_file, 'rb'))
+    else:
+        print("Creating new login")
+        ig = InstagramAPI(settings.username, settings.password)
+        ig.login()
+
+    # Make sure I don't hit the API to much
+    time.sleep(2)
+
+    ig.getProfileData()
+    print(ig.LastJson)
+    pickle.dump(ig, open(user_data_file, 'wb'))
